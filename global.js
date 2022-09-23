ce31db2785049bac3df052d7b7648578e85be3bc
Vivek Nikam <102734695+GithubVicky97@users.noreply.github.com>
2022-09-23 16:06:00 +0530

    Update global.js

--- global.js
+++ global.js
@@ -1,34 +1,31 @@
-window.onload = () =>
-  
-    {
- 
-        document.querySelector('#calculate').onclick = calculateTip;
-    }
- 
+window.onload = () => {
+  document.querySelector("#calculate").onclick = calculateTip;
+};
+
 function calculateTip() {
-   
-    let amount = document.querySelector('#amount').value;
-    let persons = document.querySelector('#persons').value;
-    let service = document.querySelector('#services').value;
- 
-    console.log(service);
+  let amount = document.querySelector("#amount").value;
+  let persons = document.querySelector("#persons").value;
+  let service = document.querySelector("#services").value;
 
-    if (amount === '' && service === 'Select') {
-        alert("Please enter valid values");
-        return;
-    }
- 
+  console.log(service);
 
-    if (persons === '1')
-    
-        document.querySelector('#each').style.display = 'none';
-    else
-        document.querySelector('#each').style.display = 'block';
- 
-   
-    let total = (amount * service) / persons;
-    total = total.toFixed(2);
- 
-    document.querySelector('.tip').style.display = 'block';
-    document.querySelector('#total').innerHTML = total;
+  if (amount === "" && service === "Select") {
+    alert("Please enter valid values");
+    return;
+  }
+
+  if (persons === "1") {
+    document.querySelector("#each").style.display = "none";
+  } else {
+    document.querySelector("#each").style.display = "block";
+  }
+
+  let total = (amount * service) / persons;
+  total = total.toFixed(2);
+  let totalAmount =((amount * service) / persons)+(amount / persons);
+  totalAmount = totalAmount.toFixed(2);
+  document.querySelector(".tip").style.display = "block";
+  document.querySelector("#total").innerHTML = total;
+  document.querySelector("#totalAmount").innerHTML = totalAmount;
+
 }
